953fdcd71210eccf51d2af49fd262a1ad1e370a2
SangSangtran <sangtuantrannguyen1211@gmail.com>
2024-04-27 10:43:57 +0700

    sql thuc hanh

---
+++ GK_cntt3_Tuấn Sang_160/b1/SQLQuery1.sql
@@ -0,0 +1,17 @@
+INSERT dbo.DMMH (MaMH,TenMH ,Sotiet) 
+VALUES('01','Co so du lieu','45')
+
+INSERT dbo.DMMH (MaMH,TenMH ,Sotiet) 
+VALUES('02','Tri tue nhan tao','45')
+
+INSERT dbo.DMMH (MaMH,TenMH ,Sotiet) 
+VALUES('03','Truyen tin','45')
+
+INSERT dbo.DMMH (MaMH,TenMH ,Sotiet) 
+VALUES('04','Do hoa','60')
+
+INSERT dbo.DMMH (MaMH,TenMH ,Sotiet) 
+VALUES('05','Van pham','60')
+
+INSERT dbo.DMMH (MaMH,TenMH ,Sotiet) 
+VALUES('06','Ky thuat lap trinh','45')
---
+++ kteam/B8 truy vấn cơ bản/Truy vấn.sql
@@ -0,0 +1,43 @@
+﻿USE HowKteam
+GO
+
+SELECT GV.MAGV, GV.HOTEN, NT.TEN  FROM dbo.GIAOVIEN AS GV, dbo.NGUOITHAN AS NT
+WHERE GV.MAGV = NT.MAGV
+
+SELECT * FROM dbo.GIAOVIEN
+WHERE PHAI = N'Nữ' AND LUONG > 2000
+
+-- Lấy ra họ tên giáo viên, năm sinh và tuổi vủa giáo viên nhỏ hơn 40
+SELECT HOTEN, NGSINH, YEAR(GETDATE()) - YEAR(NGSINH) AS 'TUOI' FROM dbo.GIAOVIEN
+WHERE YEAR(GETDATE()) - YEAR(NGSINH) <= 40
+
+-- Đếm số lượng người thân của giáo viên có mã giáo viên là 007
+SELECT COUNT(*) AS N'Số lượng người thân' 
+FROM dbo.NGUOITHAN, dbo.GIAOVIEN
+WHERE NGUOITHAN.MAGV = dbo.GIAOVIEN.MAGV AND dbo.GIAOVIEN.MAGV = N'007'
+
+-- Số lượng giáo viên khoa cntt
+SELECT COUNT(*)
+FROM dbo.GIAOVIEN AS GV, dbo.BOMON AS BM, dbo.KHOA AS K
+WHERE GV.MABM = BM.MABM 
+AND BM.MAKHOA = K.MAKHOA 
+AND K.MAKHOA = N'CNTT'
+
+--thông tin giáo viên và đề tài người đó tham gia khi mà kết quả đạt
+SELECT GV.*
+FROM dbo.GIAOVIEN AS GV, dbo.THAMGIADT AS TG
+WHERE GV.MAGV = TG.MAGV
+AND KETQUA =N'Đạt'
+
+-- Xuất ra thông tin gv mà tên có chữ l
+SELECT *
+FROM dbo.GIAOVIEN
+WHERE HOTEN LIKE N'%N%'
+
+
+
+SELECT *
+FROM dbo.GIAOVIEN LEFT JOIN dbo.BOMON
+ON BOMON.MABM = GIAOVIEN.MABM
+
+
